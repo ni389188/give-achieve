fcae96e87f0c9a54685175a42abcfac48f0900e1
ni389188 <nconeo@knights.ucf.edu>
2022-01-25 12:22:15 -0500

    Created the HomePage Route using BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
 import './App.css';
+import { render } from "react-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import HomePage from './pages/home-page';
+
 
 function App() {
-
+    render(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>,
+        document.getElementById("root")
+    );
 }
 
 export default App;
