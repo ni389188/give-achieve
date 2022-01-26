87649e6fadf83b25eb12218d7d7e6d0588b31ec9
ni389188 <nconeo@knights.ucf.edu>
2022-01-26 13:35:31 -0500

    Removed NavBar and added it to app.js

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -4,10 +4,9 @@ import NavBar from '../components/nav_bar';
 function HomePage() {
     return (
         <div>
-            <NavBar />
             <h1> Welcome to the Home Page </h1>
         </div>
-    )
+    );
 }
 
 export default HomePage;
