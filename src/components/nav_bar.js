6a7c4d31680899f85e3705b7049cc31becb5c76a
ni389188 <nconeo@knights.ucf.edu>
2022-01-26 12:03:06 -0500

    Created a Nav bar

---
+++ src/components/nav_bar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import logo from '../images/share-my-help-logo.png';
+
+function NavBar() {
+    return (
+        <Navbar sticky="top" fixed="top" bg="dark" variant="dark" expand="lg">
+            <div>
+                <Navbar.Brand>
+                    <img style={{ width: 50, height: 50 }} src={logo} alt="" />
+                </Navbar.Brand>
+            </div>
+            <Container fluid>
+                <Navbar.Brand href="#">Share My Help</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Button href="/login">Login</Button>;
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
