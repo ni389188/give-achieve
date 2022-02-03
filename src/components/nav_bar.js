7ff2cf030fba7a432b35fec36be400aa876b6167
ni389188 <nconeo@knights.ucf.edu>
2022-02-02 20:51:41 -0500

    Made login a different Nav from the other links.

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import logo from '../images/share-my-help-logo.png';
+import './nav-bar.css';
 
 function NavBar() {
     return (
-        <Navbar sticky="top" fixed="top" bg="dark" variant="dark" expand="lg">
-            <div>
-                <Navbar.Brand>
-                    <img style={{ width: 50, height: 50 }} src={logo} alt="" />
-                </Navbar.Brand>
-            </div>
-            <Container fluid>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Button href="/login">Login</Button>;
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand>
+                <img src={logo} className="nav-logo" alt="Nav bar logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className="button-links">
+                <Nav className="nav-links">
+                    <Button variant="light" href="/" className="button-links">
+                        Home
+                    </Button>
+                </Nav>
+                <Nav className="ms-auto">
+                    <Button variant="primary" href="/login" className="login-button">
+                        Login
+                    </Button>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>   
     );
 };
 
