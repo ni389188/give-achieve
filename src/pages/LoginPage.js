b7a1cfbcddf315e27e5df6f25d4dd03501c88cf9
ni389188 <nconeo@knights.ucf.edu>
2022-02-10 12:05:26 -0500

    Renamed to match the name of the LoginPage function.

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            <h1> Welcome to the Login page </ h1>
+        </ div>
+    );
+}
+
+export default LoginPage;
